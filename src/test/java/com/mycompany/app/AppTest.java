0ed32aa88b6851ff1529097c9cfecce30a3b539c
TuanHuyLe <lehuytuan20399@gmail.com>
2021-01-13 18:10:33 +0700

    Add 'Test' stage

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -35,7 +35,8 @@ public class AppTest
         App.main(null);
         try {
             assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
-        } catch (AssertionError e) {
+            assertEquals("LeTuan", "LeTuan");
+	} catch (AssertionError e) {
             fail("\"message\" is not \"Hello World!\"");
         }
     }
